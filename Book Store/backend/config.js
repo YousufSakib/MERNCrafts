b7b529f90bb580368f57221b6dc100cabe88572e
yousufSakib <yousufsakib29603@gmail.com>
2024-09-17 22:31:22 +0600

    a project update

---
+++ Book Store/backend/config.js
@@ -0,0 +1,6 @@
+export const PORT = 5555;
+
+export const mongoDBURL = "mongodb://localhost:27017";
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
